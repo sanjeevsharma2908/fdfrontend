02e16cd864eb683e9d29cb68a418e9c5c20b750e
Sanjeev Sharma <69752064+sanjeevsharma2908@users.noreply.github.com>
2024-08-13 18:26:41 +0530

    created the home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.css'
 import Header from './../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay';
 
 const Home = () => {
+  const [category,setCategory] = useState('All');
+
   return (
     <div>
       <Header />
+      <ExploreMenu category  = {category} setCategory ={setCategory} />
+      <FoodDisplay category  = {category} />
 
     </div>
   )
